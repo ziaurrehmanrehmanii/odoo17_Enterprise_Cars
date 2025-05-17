a478398e5911a93d94bdeed15a81e4b0febfc35c
ziaurehmanii <ziaurrehmanrehmani@gmail.com>
2025-05-17 16:57:05 -0700

    refactor(peas_employee): Update search method to use 'domain' instead of 'args' and adjust menu visibility logic

--- custom_addons/peas_employee/models/ir_ui_menu.py
+++ custom_addons/peas_employee/models/ir_ui_menu.py
@@ -12,29 +12,27 @@ class IrUiMenu(models.Model):
     _inherit = 'ir.ui.menu'
     
     @api.model
-    def _search(self, args, offset=0, limit=None, order=None, count=False, access_rights_uid=None):
+    def _search(self, domain, offset=0, limit=None, order=None, access_rights_uid=None, count=False):
         # If user is in peas_employee group only and not in admin/manager groups
         user = self.env.user
         _logger.info("Current User:::::::::::::::::::::::::::::::::::::::::::::::::::::::::: %s", user.name)
-        _logger.info("Initial Args:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: %s", args)
+        _logger.info("Initial Args:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: %s", domain)
 
         if user.has_group('peas_employee.group_peas_employee_user'):
             # Get Car Sales & Purchase menu ID
             car_sales_menu = self.env.ref('car_sales_purchase.menu_car_sales_purchase_root', False)
-            _logger.info("Car Sales & Purchase Menu::::::::::::::::::::::::::::::::::::::::::::::::::::: %s", car_sales_menu.id if car_sales_menu else "Not Found")
-            
+            _logger.info("Car Sales & Purchase Menu::::::::::::::::::::::::::::::::::::::::::::::::::::: %s", car_sales_menu.id if car_sales_menu else "Not Found")     
             if car_sales_menu:
                 # Only show Car Sales & Purchase menu at the root level
-                args = expression.AND([args, [('id', '=', car_sales_menu.id)]])
-                _logger.info("Updated Args::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: %s", args)
-        
+                domain = expression.AND([domain, [('id', '=', car_sales_menu.id)]])
+                _logger.info("Updated Args::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: %s", domain)
         # Use named parameters to avoid positional argument count issues
         result = super()._search(
-            args,
+            domain,
             offset=offset,
             limit=limit,
             order=order,
-            access_rights_uid=access_rights_uid
+            access_rights_uid=access_rights_uid,        
         )
         _logger.info("Search Result:::::::::::::::::::::::::::::::::::::::::::::::::::: %s", result)
         return result
